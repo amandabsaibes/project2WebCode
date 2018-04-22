5d81d1e605989d059fbb48cb96ba483b3c9c7de8
emmaleepk <emmaleepk@gmail.com>
2018-04-21 20:07:42 -0500

    Machine Learning :-)

--- dispPrediction.php
+++ dispPrediction.php
@@ -1,11 +1,15 @@
 <html>
 <body>
 <?php
+	#include '/vendor/autoload.php';
+	#use Phpml\Regression\SVR;
+
 	$host = "database.cse.tamu.edu";
 	$username = "emmaleepk";
 	$password = "csce315AAE";
 	$dbname = "emmaleepk";
-	$connection = new mysqli($host, $username, $password, $dbname);
+	$connection = mysql_connect($host, $username, $password);
+	mysql_select_db($dbname);
 	include("header.php");
 	$parkingLots = array();
 	$notParkingLots = array(2,16,17,28,29,31,39,46,52,53,56,57,105,106,116,121);
@@ -51,7 +55,110 @@
 		echo("<input type='submit'>");
 	echo("</form>");
 
+	function predictByHour()
+	{
+		$recordOfDay = array();
+		$sql = "SELECT DATE_FORMAT(`time`, '%H') as Time, COUNT(`record`) as Count FROM `Entries` GROUP BY DATE_FORMAT(`time`, '%H') ";
+		$result = mysql_query($sql);
+		if($result == false){
+			echo mysql_error();
+		}
+		while($row = mysql_fetch_assoc($result))
+		{
+			$recordOfDay[]=$row;
+			#echo $row[`record`];
+		}	
+		
+		#Train#
+		$samples = array();
+		$hours = array();
+		for($i=0; $i<sizeof($recordOfDay);$i++)
+		{		
+			array_push($samples, $recordOfDay[$i]['Count']);
+			array_push($hours, $recordOfDay[$i]['Time']);		
+		}
+		$regression = new LeastSquares();
+		$regression->train($samples, $targets);
+		echo($regression->predict([11]));
+	}
 
+	function predictByHourAndDay($hour, $day)
+	{
+		$dayHourArray = array();
+		$sqlSum = "SELECT COUNT(`record`) as SUM FROM `Entries` WHERE DATE_FORMAT(`time`, '%d') = {$day}";
+		$resultOne = mysql_query($sqlSum);
+		$sum = mysql_result($resultOne, 0);
+		$sql = "SELECT DATE_FORMAT(`time`, '%d') as Day, DATE_FORMAT(`time`, '%h')as Hour, COUNT(`record`) as Count FROM `Entries` WHERE DATE_FORMAT(`time`, '%d') ={$day} GROUP BY DATE_FORMAT(`time`, '%h')";
+		$result = mysql_query($sql);
+
+		while($row = mysql_fetch_assoc($result))
+		{
+			$dayHourArray[] = $row;
+		}
+		print_r($dayHourArray);
+		for($i=0; $i < count($dayHourArray); $i++){
+			if($dayHourArray[$i]['Hour']==$hour)
+			{
+				$hourTotal = $dayHourArray[$i]['Count'];
+				break;
+			}							
+		}
+		$average = ($sum+$hourTotal)/2;
+		echo($average);		
+	}
+
+	function predictByDayAndMonth($day, $month)
+	{
+		$monthDayArray = array();
+		$sqlSum = "SELECT COUNT(`record`) as SUM FROM `Entries` WHERE DATE_FORMAT(`time`, '%m') = {$month}";
+		$resultOne = mysql_query($sqlSum);
+		$sum = mysql_result($resultOne, 0);
+		$sql = "SELECT DATE_FORMAT(`time`, '%m') as Month, DATE_FORMAT(`time`, '%d')as Day, COUNT(`record`) as Count FROM `Entries` WHERE DATE_FORMAT(`time`, '%m') ={$month} GROUP BY DATE_FORMAT(`time`, '%d')";
+		$result = mysql_query($sql);
+
+		while($row = mysql_fetch_assoc($result))
+		{
+			$monthDayArray[] = $row;
+		}
+
+		for($i=0; $i < count($monthDayArray); $i++){
+			if($monthDayArray[$i]['Day']==$day)
+			{
+				$dayTotal = $monthDayArray[$i]['Count'];
+				break;
+			}							
+		}
+		$average = ($sum+$dayTotal)/2;
+		echo($average);
+		
+	}
+
+	function predictByMonthAndYear($month, $year)
+	{
+		$yearMonthArray = array();
+		$sqlSum = "SELECT COUNT(`record`) as SUM FROM `Entries` WHERE DATE_FORMAT(`time`, '%Y') = {$year}";
+		$resultOne = mysql_query($sqlSum);
+		$sum = mysql_result($resultOne, 0);
+		$sql = "SELECT DATE_FORMAT(`time`, '%Y') as Year, DATE_FORMAT(`time`, '%m')as Month, COUNT(`record`) as Count FROM `Entries` WHERE DATE_FORMAT(`time`, '%Y') ={$year} GROUP BY DATE_FORMAT(`time`, '%m')";
+		$result = mysql_query($sql);
+
+		while($row = mysql_fetch_assoc($result))
+		{
+			$yearMonthArray[] = $row;
+		}
+
+		for($i=0; $i < count($yearMonthArray); $i++){
+			if($yearMonthArray[$i]['Month']==$month)
+			{
+				$monthTotal = $yearMonthArray[$i]['Count'];
+				break;
+			}							
+		}
+		$average = ($sum+$monthTotal)/2;
+		echo($average);		
+	}
+	predictByMonthAndYear(04,2018);
+	
 
 ?>
 </body>
